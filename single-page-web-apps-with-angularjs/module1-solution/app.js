320a3e575b00c3e29e6a10ef5b4cd25b1cea0c17
Urban Modig <ext.urban.modig@ehalsomyndigheten.se>
2016-11-11 13:46:29 +0100

    Slight restructoring

--- single-page-web-apps-with-angularjs/module1-solution/app.js
+++ single-page-web-apps-with-angularjs/module1-solution/app.js
@@ -1,38 +1,40 @@
 (function () {
   'use strict';
 
-  angular.module('LunchCheck', [])
+  angular
+    .module('LunchCheck', [])
+    .controller('LunchCheckController', LunchCheckController);
 
-  .controller('LunchCheckController', function ($scope) {
-    $scope.lunchList = "";
-    $scope.result = "";
-    $scope.class = "alert-info";
-    $scope.hide = true;
-
-    $scope.checkLunch= function(){
-      if($scope.lunchList.length == 0){
-        alert("Please enter data first","alert-info")
-        return;
-      }
-
-      var listLenght = $scope.lunchList.split(',');
-      if(listLenght.length > 3){
-          alert("To much","alert-danger")
-      }else{
-          alert("Enjoy!","alert-success")
-      }
-    }
-
-    $scope.clear = function(){
+    function LunchCheckController($scope) {
+      $scope.lunchList = "";
+      $scope.result = "";
+      $scope.class = "alert-info";
       $scope.hide = true;
-    }
 
-    function alert(msg, severity) {
-      $scope.result = msg;
-      $scope.class = severity;
-      $scope.hide = false;
-    }
+      $scope.checkLunch= function(){
+        if($scope.lunchList.length == 0){
+          alert("Please enter data first","alert-info")
+          return;
+        }
 
-  });
+        var listLenght = $scope.lunchList.split(',');
+        if(listLenght.length > 3){
+          alert("To much","alert-danger")
+        }else{
+          alert("Enjoy!","alert-success")
+        }
+      }
+
+      $scope.clear = function(){
+        $scope.hide = true;
+      }
+
+      function alert(msg, severity) {
+        $scope.result = msg;
+        $scope.class = severity;
+        $scope.hide = false;
+      }
+
+  }
 
 })();
